b89f5b660f5d179b7ed728013468737784bde74b
RodrigoMattosoSilveira <r3850365@gmail.com>
2019-05-08 13:53:42 -0700

    WUF-41-CI Dialing for dollars

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -31,7 +31,7 @@ module.exports = function (config) {
         colors: true,
         logLevel: config.LOG_INFO,
         autoWatch: true,
-        browsers: ['Chrome'],
+        browsers: ['Chrome', 'ChromeHeadless', 'MyHeadlessChrome'],
         customLaunchers: {
             MyHeadlessChrome: {
                base: 'ChromeHeadless',
