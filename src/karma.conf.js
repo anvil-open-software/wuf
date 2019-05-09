58ed1fe2fbf00787c10fcdc4fbc0163959eb3980
RodrigoMattosoSilveira <r3850365@gmail.com>
2019-05-09 13:44:26 -0700

    WUF-41-CI-NoPuppeteer package.json version is 2.0.0-rc.11

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -6,7 +6,6 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
-process.env.CHROME_BIN = require('puppeteer').executablePath()
 module.exports = function (config) {
     config.set({
         basePath: '',
@@ -16,8 +15,7 @@ module.exports = function (config) {
             require('karma-chrome-launcher'),
             require('karma-jasmine-html-reporter'),
             require('karma-coverage-istanbul-reporter'),
-            require('@angular-devkit/build-angular/plugins/karma'),
-            require('puppeteer')
+            require('@angular-devkit/build-angular/plugins/karma')
         ],
         client: {
             clearContext: false // leave Jasmine Spec Runner output visible in browser
@@ -32,20 +30,7 @@ module.exports = function (config) {
         colors: true,
         logLevel: config.LOG_INFO,
         autoWatch: true,
-        browsers: ['Chrome', 'MyChromeHeadless'],
-        customLaunchers: {
-          MyChromeHeadless: {
-            base: 'ChromeHeadless',
-            flags: [
-            '--headless',
-            '--disable-gpu',
-            '--disable-translate',
-            '--disable-extensions',
-            '--no-sandbox',  // Added to fix an issue where of Failed to connect to chrome browser
-            '--remote-debugging-port=9222',
-            ],
-          }
-        },
+        browsers: ['Chrome'],
         singleRun: false
     });
 };
