89b91b30d265113c27ba9984ddb9b9686863ec44
RodrigoMattosoSilveira <r3850365@gmail.com>
2019-05-14 08:39:32 -0700

    Changes to make this test work: - Declared items to eliminate the 'error TS2693:  only refers to a type, but is being used as a value here.' error - Imported the Mat* modules - Imported the CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA modules

--- projects/anviltech/wuf-ang-gridster/src/lib/gridster.component.spec.ts
+++ projects/anviltech/wuf-ang-gridster/src/lib/gridster.component.spec.ts
@@ -4,10 +4,12 @@
  */
 
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA }  from '@angular/core';
 
 import { WufGridsterComponent } from './gridster.component';
 import { WufGridsterItem } from './gridster.interface';
 import { WufGridsterService } from './gridster.service';
+import { MatMenuModule, MatAutocompleteModule } from '@angular/material';
 
 
 describe('WufGridsterComponent', () => {
@@ -16,8 +18,16 @@ describe('WufGridsterComponent', () => {
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
-            declarations: [WufGridsterComponent,WufGridsterItem],
-            providers: [WufGridsterService]
+            declarations: [WufGridsterComponent],
+            imports: [
+                MatMenuModule,
+                MatAutocompleteModule
+            ],
+            providers: [WufGridsterService],
+            schemas: [
+              CUSTOM_ELEMENTS_SCHEMA,
+              NO_ERRORS_SCHEMA
+            ]
         })
         .compileComponents();
     }));
@@ -29,6 +39,7 @@ describe('WufGridsterComponent', () => {
 
     it('should create', () => {
         component = fixture.componentInstance;
+        component.items;
         expect(component).toBeTruthy();
     });
 });
