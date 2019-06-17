c12d7855478fb5aa478c1fc69042e31e6ebb5fff
RodrigoMattosoSilveira <r3850365@gmail.com>
2019-06-17 11:17:33 -0700

    WUF-84-LERNA-publish  -  Moved all scripts to script files.

---
+++ .circleci/deploy.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+var=$(pwd)
+
+echo "Start of Deploy WUF: CWD $var."
+if [ $CIRCLE_BRANCH == $DEPLOY_BRANCH ]; then
+   git config --global user.email $GH_EMAIL
+   git config --global user.name $GH_NAME
+
+   git clone $CIRCLE_REPOSITORY_URL out
+
+   cd out
+   git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
+   git rm -rf .
+   cd ..
+
+   cp -a dist/. out/.
+
+   mkdir -p out/.circleci && cp -a .circleci/. out/.circleci/.
+   cd out
+
+   git add -A
+   git commit -m "Automated deployment to GitHub Pages: ${CIRCLE_SHA1} [skip ci]" --allow-empty
+
+   git push origin $TARGET_BRANCH
+
+   var=$(pwd)
+   echo "Done deploying WUF: CWD $var."
+   cd ..
+   var=$(pwd)
+   echo "Done going bach to orginal dir: CWD $var."
+fi
