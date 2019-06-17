02480d1ca7084ce7c32fc14f4f784089d1e312fb
RodrigoMattosoSilveira <r3850365@gmail.com>
2019-06-17 15:09:11 -0700

    WUF-84-LERNA-publish  -  Lets try again

--- .circleci/publish.sh
+++ .circleci/publish.sh
@@ -3,9 +3,9 @@
 if [ $CIRCLE_BRANCH == $DEPLOY_BRANCH ]; then
    var=$(pwd)
    echo "Start of publish WUF packages: CWD = $var."
-   npm whoami
-   echo "//https://www.npmjs.com/:_authToken=$NPM_TOKEN" > ./.npmrc
+   npm set //registry.npmjs.org/:_authToken=$NPM_TOKEN
    cat ./.npmrc
+   npm whoami
    git reset --hard # removes staged and working directory changes
    lerna publish from-package --yes
 fi
