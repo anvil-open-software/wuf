b3798f0efd642175348ea565b619b66363c87c3d
RodrigoMattosoSilveira <r3850365@gmail.com>
2019-07-09 15:28:46 -0700

    wuf-92-broken_build  -  I appended the build number in the wrong number

--- .circleci/tag.sh
+++ .circleci/tag.sh
@@ -28,8 +28,8 @@ if is_cicrcleci_branch_master || is_feature_branch_version; then
    # Set the tag to be the package.json version
    # For branches other than master, append an unique value to ...
    # provent build failures.
-   GITTAG=v$(npx -c 'echo "$npm_package_version"')
-   echo Taging $CIRCLE_BRANCH: $GITTAG.$CIRCLE_BUILD_NUM
+   GITTAG=v$(npx -c 'echo "$npm_package_version"').$CIRCLE_BUILD_NUM
+   echo Taging $CIRCLE_BRANCH: $GITTAG
 
    # Using annotated tags; required to include tag to prevent build from running
    # when checking in the tag [https://circleci.com/docs/2.0/skip-build/]
